9dfc8224197ed7ff0f192d79010c656cafb6dad6
Magdalena Kołtuniak <m.koltuniak@gmail.com>
2026-06-08 19:06:10 +0200

    add task 1

--- src/components/Alert/Alert.styled.js
+++ src/components/Alert/Alert.styled.js
@@ -1,7 +1,27 @@
 import styled from 'styled-components';
 
+const defaultTheme = {
+	color: '#000',
+	bg: '#f8f9fa',
+	border: '#dcdcdc',
+};
+
 const StyledAlert = styled.div`
-    display: block;
-`
+	display: block;
+	border-radius: 4px;
+	color: var(--color-alfa);
+	padding: 12px 16px;
+
+	${({ theme, variant }) => {
+		const themeVariant = theme?.[variant];
+		const final = themeVariant || defaultTheme;
+
+		return `
+            --color-alfa: ${final.color};
+            background-color: ${final.bg};
+	        border: 1px solid ${final.border};
+        `;
+	}}
+`;
 
 export { StyledAlert };
---
+++ src/components/styled/theme.js
@@ -0,0 +1,14 @@
+const themeSettings = {
+    primary: {
+        color: '#004085',
+        bg: '#cce5ff',
+        border: '#b8daff',
+    },
+    secondary: {
+        color: '#383d41',
+        bg: '#e2e3e5',
+        border: '#d6d8db',
+    }	
+};
+
+export default themeSettings;
